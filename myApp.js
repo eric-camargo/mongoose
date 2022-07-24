979710aaa10dee4b8f768c24324e52ec7561aaa4
Eric <ericbobcamargo@gmail.com>
2022-07-24 18:59:13 -0300

    Query Chaining

--- myApp.js
+++ myApp.js
@@ -100,8 +100,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({name: 1, age: 0, favoriteFoods: 1}).exec((err, data) => {
+    if (err) return console.log(err)
+    done(err, data)
+  })
 };
 
 /** **Well Done !!**
