3fba82d2a95fca5fe742bc9cd6a9f6182ee3d01a
Eric <ericbobcamargo@gmail.com>
2022-07-24 18:47:22 -0300

    Find and Remove Person

--- myApp.js
+++ myApp.js
@@ -84,7 +84,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({id: personId}, (err, removedPerson) => {
+    if (err) return console.log(err)
+    done(null, removedPerson)
+  })
 };
 
 const removeManyPeople = (done) => {
